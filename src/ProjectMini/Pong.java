a8e434244d49d4724251780b7a9e6820a6212b1b
mokana99 <40238135+mokana99@users.noreply.github.com>
2020-04-20 14:57:48 +0200

    Add files via upload

---
+++ src/ProjectMini/Pong.java
@@ -0,0 +1,17 @@
+package ProjectMini;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Pong extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+       
+    }
+    
+}
